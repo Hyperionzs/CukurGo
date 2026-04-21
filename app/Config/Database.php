06b0c05f2a65d9da9369fd020e2bcfe0e6dd9823
Hyperionzs <hyperions004@gmail.com>
2026-04-21 17:37:34 +0700

    feat: add Database configuration class with environment variable support

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,11 +1,23 @@
 <?php
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
 class Database {
-    private $host = "localhost";
-    private $db_name = "cukurgo";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     public $conn;
+
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->db_name = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+    }
 
     public function getConnection() {
         $this->conn = null;
